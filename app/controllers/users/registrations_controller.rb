85feb799188a0a0fd1103f59dc619d732cf7b7e4
Kyohei Seo <kyo199333@gmail.com>
2016-03-07 02:57:24 +0900

    password無しでプロフィールを編集できるようにした

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 # before_filter :configure_sign_up_params, only: [:create]
 # before_filter :configure_account_update_params, only: [:update]
+before_action :configure_permitted_parameters, only: [:create, :update]
 
   # GET /resource/sign_up
   # def new
@@ -18,14 +19,35 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    #if update_resource(resource, account_update_params)
+    if resource.update_without_current_password(account_update_params)
+      yield resource if block_given?
+      if is_flashing_format?
+        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+          :update_needs_confirmation : :updated
+        set_flash_message :notice, flash_key
+      end
+      sign_in resource_name, resource, :bypass => true
+      respond_with resource, :location => after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
 
   # DELETE /resource
   # def destroy
   #   super
   # end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:nickname, :work, :profile, :avatar]
+    devise_parameter_sanitizer.for(:account_update) << [:nickname, :work, :profile, :avatar]
+  end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
