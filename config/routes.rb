c89e0ea61dc0cde5c95074926fc28642670cef44
Kyohei Seo <kyo199333@gmail.com>
2016-03-07 03:28:19 +0900

    スペースを追加した

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations', account_update: 'users/registrations'}
+  devise_for :users, controllers: { registrations: 'users/registrations', account_update: 'users/registrations' }
   root 'users#index'
 end
