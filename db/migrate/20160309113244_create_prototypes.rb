65ea8e727ce8e1e4248b6819fc9bb9c2c2ddac63
Kyohei Seo <kyo199333@gmail.com>
2016-03-09 22:05:12 +0900

    カラム名を変更するためにrollbackを行った カラム名を変更後再度マイグレーションファイルを実行した

--- db/migrate/20160309113244_create_prototypes.rb
+++ db/migrate/20160309113244_create_prototypes.rb
@@ -1,8 +1,11 @@
 class CreatePrototypes < ActiveRecord::Migration
   def change
     create_table :prototypes do |t|
-
-      t.timestamps null: false
+      t.string :title
+      t.string :catch_copy
+      t.text :concept
+      t.integer :user_id
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160305113728) do
+ActiveRecord::Schema.define(version: 20160309113244) do
+
+  create_table "prototypes", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.string   "catch_copy", limit: 255
+    t.text     "concept",    limit: 65535
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "nickname",               limit: 255
