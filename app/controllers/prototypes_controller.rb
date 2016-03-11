ad937287ce1a7a7dccfa77cd4a25d5603911bbdd
Kyohei Seo <kyo199333@gmail.com>
2016-03-11 21:35:17 +0900

    fields_forを使い、プロトタイプの画像を保存できるようにした

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -4,5 +4,15 @@ class PrototypesController < ApplicationController
 
   def new
     @prototype = Prototype.new
+    @prototype.images.build
+  end
+
+  def create
+    @prototype = Prototype.create(prototype_params)
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, images_attributes: [:id, :image])
   end
 end
