29dd3499d0ae18b20130a0ff8bd0554e49b9d9b0
Kyohei Seo <kyo199333@gmail.com>
2016-03-18 19:46:50 +0900

    投稿された画像をメインとサブで分けるためのカラムを追加した

--- db/migrate/20160310113712_create_images.rb
+++ db/migrate/20160310113712_create_images.rb
@@ -3,6 +3,7 @@ class CreateImages < ActiveRecord::Migration
     create_table :images do |t|
       t.string :image
       t.integer :prototype_id
+      t.integer :status
       t.timestamps
     end
   end
