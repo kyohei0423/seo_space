086d5da24d843ea9d29701de6587a7090890f0df
Kyohei Seo <kyo199333@gmail.com>
2016-03-18 19:47:02 +0900

    マイグレーションを実行した

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160310113712) do
+ActiveRecord::Schema.define(version: 20160311140261) do
 
   create_table "images", force: :cascade do |t|
     t.string   "image",        limit: 255
     t.integer  "prototype_id", limit: 4
+    t.integer  "status",       limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -28,6 +29,26 @@ ActiveRecord::Schema.define(version: 20160310113712) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id",        limit: 4
+    t.integer  "taggable_id",   limit: 4
+    t.string   "taggable_type", limit: 255
+    t.integer  "tagger_id",     limit: 4
+    t.string   "tagger_type",   limit: 255
+    t.string   "context",       limit: 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string  "name",           limit: 255
+    t.integer "taggings_count", limit: 4,   default: 0
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "nickname",               limit: 255
