b546f551bb104532feb69ab2953732f2fe21fe01
Kyohei Seo <kyo199333@gmail.com>
2016-04-05 00:37:05 +0900

    pathで統一した

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -16,7 +16,7 @@ class PrototypesController < ApplicationController
   def create
     @prototype =  current_user.prototypes.new(prototype_params)
     if @prototype.save
-      redirect_to root_url
+      redirect_to root_path
     else
       render :new
     end
@@ -28,7 +28,7 @@ class PrototypesController < ApplicationController
 
   def update
     if @prototype.update(prototype_params)
-      redirect_to root_url
+      redirect_to root_path
     else
       render :edit
     end
@@ -36,7 +36,7 @@ class PrototypesController < ApplicationController
 
   def destroy
     @prototype.destroy
-    redirect_to root_url
+    redirect_to root_path
   end
 
   private
