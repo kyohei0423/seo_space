24f0f6e45691a8bdda94a29d8c2f8d54894e7752
Kyohei Seo <kyo199333@gmail.com>
2016-04-06 00:36:29 +0900

    空行を入れた

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,7 +1,9 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :images, dependent: :delete_all
+  
   accepts_nested_attributes_for :images
+
   acts_as_taggable
 
   validates :title, :catch_copy, :concept, presence: true
