5b364fd4879a6fc17e43ed3dfe91dd4fab615de6
Kyohei Seo <kyo199333@gmail.com>
2016-04-16 17:30:27 +0900

    likeを削除した場合に数が反映されていなかったので修正した。 - prototypeの取得を削除後に行うようにした。 - destroyの場合はbefore_actionを呼ばないようにした。 - メソッド名の修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,17 @@
 class LikesController < ApplicationController
-  before_action :set_proto_type, only: [:create, :destroy]
+  before_action :set_prototype, only: :create
 
   def create
-    @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
+    @like = @prototype.likes.create(user_id: current_user.id)
   end
 
   def destroy
-    Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id]).destroy
+    Like.find_by(prototype_id: params[:prototype_id], user_id: current_user.id).destroy
+    set_prototype
   end
 
   private
-  def set_proto_type
+  def set_prototype
     @prototype = Prototype.find(params[:prototype_id])
   end
 end
