ae4fb0c3f8eb161d767011dfe0ad25837c83c327
Kyohei Seo <kyo199333@gmail.com>
2016-04-17 12:08:44 +0900

    tag関連のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'prototypes#index'
   devise_for :users, controllers: { registrations: 'users/registrations', account_update: 'users/registrations' }
   resources :users, only: :show
+  resources :tags, only: [:index, :show]
   resources :prototypes, only: [:show, :new, :create, :edit, :update, :destroy] do
     resources :comments, only: :create
     resources :likes, only: [:create, :destroy]
