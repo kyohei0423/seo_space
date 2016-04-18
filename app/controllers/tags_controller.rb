4fe8da4927c6918039d3785c780242238e45dfbb
Kyohei Seo <kyo199333@gmail.com>
2016-04-19 01:56:18 +0900

    add show page(tags#show) - add show action - add and edit show.html.ham - add and partial templete

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,5 +3,8 @@ class TagsController < ApplicationController
     @tags = ActsAsTaggableOn::Tag.most_used.where('taggings_count > 0')
   end
 
+  def show
+    tag = ActsAsTaggableOn::Tag.find(params[:id])
+    @prototypes = Prototype.tagged_with(tag).includes(:user).includes(:tags)
   end
 end
