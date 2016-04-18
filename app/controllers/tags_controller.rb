85958ebbe58c6d3378580d09f043257ccb11598b
Kyohei Seo <kyo199333@gmail.com>
2016-04-18 22:00:41 +0900

    1つもプロトタイプが紐付いていないタグは取得しないようにした。

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,7 @@
 class TagsController < ApplicationController
   def index
-    @tags = ActsAsTaggableOn::Tag.most_used
+    @tags = ActsAsTaggableOn::Tag.most_used.where('taggings_count > 0')
+  end
+
   end
 end
