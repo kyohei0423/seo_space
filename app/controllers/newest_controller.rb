f366a3037f51e842fc95a6a7063eb4bc565dac76
Kyohei Seo <kyo199333@gmail.com>
2016-04-19 23:56:43 +0900

    fix commented points

--- app/controllers/newest_controller.rb
+++ app/controllers/newest_controller.rb
@@ -1,6 +1,6 @@
 class NewestController < ApplicationController
   def index
-    @prototypes = Prototype.order('created_at desc').includes(:user, :tags)
+    @prototypes = Prototype.order(created_at: :desc).includes(:user, :tags)
     render template: "prototypes/index"
   end
 end
--- app/controllers/popularest_controller.rb
+++ app/controllers/popularest_controller.rb
@@ -1,6 +1,6 @@
 class PopularestController < ApplicationController
   def index
-    @prototypes = Prototype.order('likes_count desc').includes(:user, :tags)
+    @prototypes = Prototype.order(likes_count: :desc).includes(:user, :tags)
     render template: "prototypes/index"
   end
 end
