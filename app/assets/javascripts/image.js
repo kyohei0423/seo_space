9202af1bfd13f2570adb58233444705841ec6d57
Kyohei Seo <kyo199333@gmail.com>
2016-04-26 02:07:38 +0900

    Add immediately reflect preview images
    edit javascript file
    add class in prototypes/_prototype_form.html.haml

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -2,9 +2,30 @@ $(function() {
   var i = 0;
   $('.add-image-form').on('click', function() {
     $(this).addClass('default-hidden')
-    console.log(i);
     $('.add-image-form').eq(i + 1).removeClass('default-hidden')
     $('.appear_form').eq(i).removeClass('default-hidden')
     i++
+  })
+
+  $('.image-upload').each(function() {
+    var self = $(this);
+    var input = self.find('input[type=file]');
+    input.change(function() {
+      var file = $(this).prop('files')[0];
+      var fileReader = new FileReader();
+      if (!this.files.length) {
+        return
+      }
+      if (!file.type.match('image.*')) {
+        return
+      }
+      fileReader.onload = function() {
+        self.css({
+          'background-image': 'url(' + fileReader.result + ')',
+          'background-size': 'contain'
+        });
+      };
+      fileReader.readAsDataURL(file);
+    })
   })
 })
