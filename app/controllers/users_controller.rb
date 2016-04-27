d3e7be12faa729b755fd99c0a0aa49f58fe4db4b
Kyohei Seo <kyo199333@gmail.com>
2016-04-28 00:47:44 +0900

    Add user page
    - add show action and edit
    - add view file for show
    - add path to user page
    - fix layout file to apply show file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  before_action :set_user
+
+  def show
+    @prototypes = @user.prototypes.includes(:tags).page(params[:page]).per(10)
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
