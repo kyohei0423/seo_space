535c6db78c30bb8cfff6ecf777298e3adc3e8e2e
Kyohei Seo <kyo199333@gmail.com>
2016-04-27 23:44:39 +0900

    Set display number of per page to show prototypes

--- app/controllers/newest_controller.rb
+++ app/controllers/newest_controller.rb
@@ -1,6 +1,6 @@
 class NewestController < ApplicationController
   def index
-    @prototypes = Prototype.order(created_at: :desc).includes(:user, :tags)
+    @prototypes = Prototype.order(created_at: :desc).includes(:user, :tags).page(params[:page]).per(10)
     render template: "prototypes/index"
   end
 end
--- app/controllers/popularest_controller.rb
+++ app/controllers/popularest_controller.rb
@@ -1,6 +1,6 @@
 class PopularestController < ApplicationController
   def index
-    @prototypes = Prototype.order(likes_count: :desc).includes(:user, :tags)
+    @prototypes = Prototype.order(likes_count: :desc).includes(:user, :tags).page(params[:page]).per(10)
     render template: "prototypes/index"
   end
 end
